423ad12899dabe64ce8ef89fc6ed546ddc3bbcc1
Nicoanik <Nicoanik@hotmail.com>
2025-04-26 21:47:38 +0300

    26.04.2025

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,32 @@
+import java.awt.Menu
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Добро пожаловать!\nДавайте сохраним ваши мысли! :)")
+    val menu = Menu()
+    menu.
+}
+
+class Menu{
+    private val arhivesMenu = mapOf(
+        0 to "Создать архив",
+        1 to "Открыть архив",
+        2 to "Выйти из программы"
+    )
+
+    private val notesMenu = mapOf(
+        0 to "Создать заметку",
+        1 to "Открыть заметку",
+        2 to "Вернуться назад"
+    )
+
+    private val notesActionMenu = mapOf(
+        0 to "Редактировать заметку",
+        1 to "Прочитать заметку",
+        2 to "Вернутся назад"
+    )
+    fun printMenu(menu: Map<Int,String>) {
+        for(key in menu) {
+            println("'$key' - $menu")
+        }
+    }
 }
